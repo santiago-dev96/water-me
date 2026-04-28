1417a6b71abf0029efd156f42ac30d1fa916ba17
Santiago Rodriguez <santo1996.29@gmail.com>
2026-04-27 20:31:22 -0400

    Defined database module

---
+++ db.py
@@ -0,0 +1,30 @@
+import os
+import sqlite3
+from flask import g
+
+DATABASE = os.getenv('DATABASE')
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def init_db(app):
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def close_db():
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
