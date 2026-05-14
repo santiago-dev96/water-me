dfbaa59f6d008ca4bea51a0f8b93e371d350f244
Santiago Rodriguez <santo1996.29@gmail.com>
2026-05-14 18:11:06 -0400

    Started fixing proyect layout

---
+++ flaskr/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_prefixed_env()
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, 'app_db.sqlite'),
+        IMAGES=os.path.join(app.instance_path, 'images')
+    )
+
+    if test_config is not None:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    os.makedirs(app.instance_path, exist_ok=True)
+    os.makedirs(os.path.join(app.instance_path, app.config['IMAGES']), exist_ok=True)
+
+    from . import db
+    db.init_app(app)
+
+    return app
+
