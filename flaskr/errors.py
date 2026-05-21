4d6e22d1ff6d9ef2c88b861d2c99b869baa56ade
Santiago Rodriguez <santo1996.29@gmail.com>
2026-05-21 19:05:22 -0400

    Added 404 error page

--- flaskr/errors.py
+++ flaskr/errors.py
@@ -2,6 +2,10 @@ from flask import render_template
 
 
 def setup_error_handlers(app):
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template("errors/404.html"), 404
+
     @app.errorhandler(500)
     def internal_server_error(error):
         app.logger.error(f"Internal server error: {error}")
