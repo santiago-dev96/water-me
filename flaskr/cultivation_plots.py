a62c5d6eb20fc9a188c79501525a0ba3efba7d99
Santiago Rodriguez <santo1996.29@gmail.com>
2026-06-22 16:53:28 -0400

    Create a route to view a cultivation plot

--- flaskr/cultivation_plots.py
+++ flaskr/cultivation_plots.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, g, render_template, request, flash, redirect, url_for, current_app
+    Blueprint, g, render_template, request, flash, redirect, url_for, current_app, abort
 )
 
 from flaskr.db import get_db
@@ -28,17 +28,17 @@ def new_cultivation_plot():
 
     if request.method == "GET":
         return render_template("cultivation_plots/new_cultivation_plot.html")
-    
+
     name = request.form.get('name')
     if not name:
         flash('The name of the plot is required', 'danger')
         return render_template("cultivation_plots/new_cultivation_plot.html", form=request.form), 400
-    
+
     crop = request.form.get('crop')
     if not crop:
         flash('The crop is required', 'danger')
         return render_template("cultivation_plots/new_cultivation_plot.html", form=request.form), 400
-    
+
     number_of_plants = request.form.get('number-of-plants')
     if not number_of_plants:
         number_of_plants = 0
@@ -52,7 +52,7 @@ def new_cultivation_plot():
         except ValueError:
             current_app.logger.info(bad_value_msg)
             number_of_plants = 0
-    
+
     db = get_db()
     user_id = g.user['id']
     cursor = db.execute('INSERT INTO cultivation_plots (name, crop, number_of_plants, user_id) VALUES (?, ?, ?, ?)', (name, crop, number_of_plants, user_id))
@@ -60,3 +60,16 @@ def new_cultivation_plot():
     db.commit()
 
     return redirect(url_for('cultivation_plots.index'))
+
+
+@bp.route('/cultivation_plots/<int:id>', methods=["GET"])
+@login_required
+def cultivation_plot(id):
+    db = get_db()
+    cursor = db.execute('SELECT * FROM cultivation_plots WHERE id = ?', (id,))
+    cultivation_plot = cursor.fetchone()
+    cursor.close()
+    if not cultivation_plot:
+        return abort(404)
+    else:
+        return render_template('cultivation_plots/cultivation_plot.html', cultivation_plot=cultivation_plot)
