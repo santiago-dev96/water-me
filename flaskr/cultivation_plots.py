4694dcc5a6e5ade7358e0165b109186c781c1148
Santiago Rodriguez <santo1996.29@gmail.com>
2026-06-26 11:22:14 -0400

    Improve template names and define operations view

--- flaskr/cultivation_plots.py
+++ flaskr/cultivation_plots.py
@@ -18,7 +18,7 @@ def index():
     cursor = db.execute("SELECT * FROM cultivation_plots WHERE user_id = ?", (user_id,))
     cultivation_plots = cursor.fetchall()
     cursor.close()
-    return render_template("cultivation_plots/index.html.jinja", cultivation_plots=cultivation_plots)
+    return render_template("cultivation_plots/dashboard.html.jinja", cultivation_plots=cultivation_plots)
 
 
 @bp.route("/new_cultivation_plot", methods=["GET", "POST"])
@@ -27,17 +27,17 @@ def new_cultivation_plot():
     """New cultivation plot."""
 
     if request.method == "GET":
-        return render_template("cultivation_plots/new_cultivation_plot.html.jinja")
+        return render_template("cultivation_plots/new.html.jinja")
 
     name = request.form.get('name')
     if not name:
         flash('The name of the plot is required', 'danger')
-        return render_template("cultivation_plots/new_cultivation_plot.html.jinja", form=request.form), 400
+        return render_template("cultivation_plots/new.html.jinja", form=request.form), 400
 
     crop = request.form.get('crop')
     if not crop:
         flash('The crop is required', 'danger')
-        return render_template("cultivation_plots/new_cultivation_plot.html.jinja", form=request.form), 400
+        return render_template("cultivation_plots/new.html.jinja", form=request.form), 400
 
     number_of_plants = request.form.get('number-of-plants')
     if not number_of_plants:
@@ -72,7 +72,7 @@ def cultivation_plot(id):
     if not cultivation_plot:
         return abort(404)
     else:
-        return render_template('cultivation_plots/cultivation_plot.html.jinja', cultivation_plot=cultivation_plot)
+        return render_template('cultivation_plots/status.html.jinja', cultivation_plot=cultivation_plot)
 
 
 @bp.route('/cultivation_plots/<int:id>/log_operation', methods=["GET", "POST"])
@@ -85,26 +85,26 @@ def log_cultivation_plot_operation(id):
     if not cultivation_plot:
         return abort(404)
     elif request.method == 'GET':
-        return render_template('cultivation_plots/log_cultivation_plot_operation.html.jinja', cultivation_plot=cultivation_plot)
+        return render_template('cultivation_plots/log_operation.html.jinja', cultivation_plot=cultivation_plot)
     else:
         number_of_plants = request.form.get('number_of_plants', 0)
         try:
             number_of_plants = int(number_of_plants)
         except ValueError:
             flash('The number of plants must be a whole number', 'danger')
-            return render_template('cultivation_plots/log_cultivation_plot_operation.html.jinja', cultivation_plot=cultivation_plot), 400
+            return render_template('cultivation_plots/log_operation.html.jinja', cultivation_plot=cultivation_plot), 400
         water_spent = request.form.get('water_spent', 0.0)
         try:
             water_spent = float(water_spent)
         except ValueError:
             flash('The amount of water must be a number greater than zero', 'danger')
-            return render_template('cultivation_plots/log_cultivation_plot_operation.html.jinja', cultivation_plot=cultivation_plot), 400
+            return render_template('cultivation_plots/log_operation.html.jinja', cultivation_plot=cultivation_plot), 400
         harvest = request.form.get('harvest', 0.0)
         try:
             harvest = float(harvest)
         except ValueError:
             flash('The harvest must be a number greater than zero', 'danger')
-            return render_template('cultivation_plots/log_cultivation_plot_operation.html.jinja', cultivation_plot=cultivation_plot), 400
+            return render_template('cultivation_plots/log_operation.html.jinja', cultivation_plot=cultivation_plot), 400
         cursor = db.execute('INSERT INTO operations (number_of_plants, water_spent, harvest, cultivation_plot_id) VALUES (?, ?, ?, ?)', (number_of_plants, water_spent, harvest, id))
         cursor.close()
         cursor = db.execute('UPDATE cultivation_plots SET number_of_plants = ?, water_spent = ?, harvest = ? WHERE id = ?', (cultivation_plot['number_of_plants'] + number_of_plants, cultivation_plot['water_spent'] + water_spent, cultivation_plot['harvest'] + harvest, id))
@@ -112,3 +112,13 @@ def log_cultivation_plot_operation(id):
         db.commit()
         flash('Operation successfully logged', 'success')
         return redirect(url_for('cultivation_plots.cultivation_plot', id=id))
+
+
+@bp.route('/cultivation_plots/<int:id>/operations', methods=['GET'])
+@login_required
+def cultivation_plot_operations(id):
+    db = get_db()
+    cursor = db.execute('SELECT * FROM operations WHERE cultivation_plot_id = ?', (id,))
+    operations = cursor.fetchall()
+    cursor.close()
+    return render_template('cultivation_plots/operations.html.jinja', operations=operations)
